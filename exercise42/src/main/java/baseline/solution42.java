ae5e8ffc4f968c2fec97fcd6bd72682a38e9bd68
Benjaminshin <Benjaminshin92@gmail.com>
2021-10-12 21:02:04 -0400

    psuedocode

---
+++ exercise42/src/main/java/baseline/solution42.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Benjamin Shin
+ */
+/*
+Sometimes data comes in as a structured format that you have to break down and turn into records so you can
+process them. CSV, or comma-separated values, is a common standard for doing this.
+*/
+package baseline;
+
+public class solution42 {
+
+    public void parse_input(){
+        //will parse the data into separate strings by splitting up the information based on the comma
+    }
+
+    
+    public static void main(String[] args) {
+        solution42 app =new solution42();
+
+    }
+}
+
